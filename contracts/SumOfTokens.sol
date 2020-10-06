59e394233da9861700ab8724a0fb775236e2fc29
Victor Porton <porton@narod.ru>
2020-10-06 14:06:44 +0300

    initial contract version

---
+++ contracts/SumOfTokens.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: Apache-2.0	
+pragma solidity ^0.7.1;
+
+import "./ERC1155.sol";
+
+// A sample implementation of core ERC1155 function.
+contract SumOfToken is ERC1155
+{
+}
