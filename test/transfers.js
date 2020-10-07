770a6ab1633eb1f4f5c5e9dd98a862df698ca11f
Victor Porton <porton@narod.ru>
2020-10-07 14:24:46 +0300

    more tests

--- test/transfers.js
+++ test/transfers.js
@@ -101,8 +101,12 @@ describe("SumOfTokens", function() {
             const result = await sumOfTokens.balanceOf(to.address, t);
             newToBalances.push(ethers.BigNumber.from(result));
           }
+          for(let i = 0; i < newFromBalances.length; ++i) {
+            const change = oldFromBalances[i].sub(newFromBalances[i]);
+            expect(change).to.equal(amount);
+          }
           for(let i = 0; i < newToBalances.length; ++i) {
-            const change = newToBalances[i].sub(oldToBalances[i]); // TODO: Check from balances, too
+            const change = newToBalances[i].sub(oldToBalances[i]);
             expect(change).to.equal(amount);
           }
         } else {
