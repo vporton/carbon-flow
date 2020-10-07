03db87e1023ea9414a54a098f7345c3e588151f2
Victor Porton <porton@narod.ru>
2020-10-07 14:25:21 +0300

    contract simplified

--- contracts/SumOfTokens.sol
+++ contracts/SumOfTokens.sol
@@ -37,7 +37,7 @@ contract SumOfTokens is ERC1155, IERC1155Views
         require(tokenOwners[_id] == msg.sender);
         // require(_id != 0);
 
-        require(_to != address(address(0)), "_to must be non-zero.");
+        require(_to != address(0), "_to must be non-zero.");
 
         _doMint(_to, _id, _value);
 
@@ -71,7 +71,7 @@ contract SumOfTokens is ERC1155, IERC1155Views
     function safeTransferFrom(address _from, address _to, uint256 _id, uint256 _value, bytes calldata _data) external override {
         require(_id != 0);
 
-        require(_to != address(address(0)), "_to must be non-zero.");
+        require(_to != address(0), "_to must be non-zero.");
         require(_from == msg.sender || _allowance(_id, _from, msg.sender) >= _value);
 
         require(_doTransferFrom(_from, _to, _id, _value) == _value);
@@ -89,7 +89,7 @@ contract SumOfTokens is ERC1155, IERC1155Views
     function safeBatchTransferFrom(address _from, address _to, uint256[] calldata _ids, uint256[] calldata _values, bytes calldata _data) external override {
 
         // MUST Throw on errors
-        require(_to != address(address(0)), "destination address must be non-zero.");
+        require(_to != address(0), "destination address must be non-zero.");
         require(_ids.length == _values.length, "_ids and _values array length must match.");
         if(_from != msg.sender) {
             for (uint256 i = 0; i < _ids.length; ++i) {
