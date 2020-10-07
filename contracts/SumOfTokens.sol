d8a9be9ef6a2627f317c66906677df3773f36f21
Victor Porton <porton@narod.ru>
2020-10-07 23:07:03 +0300

    bug fix

--- contracts/SumOfTokens.sol
+++ contracts/SumOfTokens.sol
@@ -278,8 +278,8 @@ contract SumOfTokens is ERC1155, IERC1155Views
             _prevAddr = _childAddr;
             _childAddr = _childToken.next;
         }
-        // return (_value - _remainingValue, _remainingValue != 0);
-        return (_value - _remainingValue, _childAddr != 0);
+        return (_value - _remainingValue, _remainingValue != 0);
+        // return (_value - _remainingValue, _childAddr != 0);
     }
 
 // Events
