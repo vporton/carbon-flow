d9ff613604b9eec08b641ed2c102ed4ff65e072b
Victor Porton <porton@narod.ru>
2020-10-10 02:25:34 +0300

    removed logging

--- test/smart-wallet.js
+++ test/smart-wallet.js
@@ -42,7 +42,6 @@ describe("SmartWallet", function() {
       const smartWallet = new SmartWallet();
       const contract = new ethers.Contract(deployResult.address, deployResult.abi, walletOwners[i]);
       await smartWallet.init(contract);
-      console.log(await smartWallet.owner());
       smartWallets.push(smartWallet);
     }
   });
