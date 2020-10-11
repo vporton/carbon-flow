0c4b8f06cead6f62ec8aaf26ab571daee750a68e
Victor Porton <porton@narod.ru>
2020-10-11 10:53:04 +0300

    UI shows transactions

--- ui/js/index.js
+++ ui/js/index.js
@@ -25,23 +25,41 @@ async function onLoad() {
     carbon.methods.balanceOf(defaultAccount, nonRetiredCreditsToken).call().then(value => {
         document.getElementById('nonretired').textContent = web3.utils.fromWei(value);
     });
+    carbon.methods.tax().call().then(value => {
+        document.getElementById('tax').textContent = value / 2**64 * 100;
+    });
 }
 
 async function retire() {
     const amountStr = document.getElementById('amount').value;
-    const amount = web3.utils.toWei(amountStr.replace(/^ *| *$/g, ''));
-    await doRetire(amount);
+    await doRetire(amountStr);
 }
 
-async function doRetire(amount) {
+async function doRetire(amountStr) {
     const carbon = new web3.eth.Contract(await carbonJsonInterface(), carbonAddress);
-    await mySend(carbon, carbon.methods.retireCredit, [amount])
+    const amount = web3.utils.toWei(amountStr);
+    const tx = mySend(carbon, carbon.methods.retireCredit, [amount])
         .catch(e => {
             if(!(e.code === 4001)) {
                 alert("Error: Do you have sufficient M- tokens for this operation?");
             }
         });
-    // TODO: Display current operations
+    const li = document.getElementById('transactionTmpl').cloneNode(true);
+    const count = document.getElementById('transactions').childNodes.length;
+    const id = `transaction-${count}`;
+    li.setAttribute('id', id);
+    document.getElementById('transactions').appendChild(li);
+    document.querySelector(`#${id} .tokens`).textContent = amountStr;
+    li.style.display = 'block';
+
+    tx.then((receipt, error) => {
+        const li = document.getElementById(id);
+        li.className = receipt && receipt.status ? 'transaction confirmed' : 'transaction failed';
+        document.querySelector(`#${id} .status`).textContent = !receipt ? 'rejected' : receipt.status ? 'confirmed' : 'failed';
+        if(receipt) {
+            document.querySelector(`#${id} .hash`).textContent = receipt.transactionHash;
+        }
+    });
 }
 
 function validateAmount() {
