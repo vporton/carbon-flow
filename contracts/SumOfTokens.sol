65532928e2d999def356a67eef8024a6e6aea4af
Victor Porton <porton@narod.ru>
2020-10-11 05:40:18 +0300

    TODO clearance

--- contracts/SumOfTokens.sol
+++ contracts/SumOfTokens.sol
@@ -224,7 +224,7 @@ contract SumOfTokens is ERC1155, IERC1155Views
         if(_value != 0) {
             _updateUserTokens(_to, _id, _value);
             do {
-                totalSupplyImpl[_id] += _value; // TODO: Should decrease on transfer to 0x0? // FIXME: overflow checking
+                totalSupplyImpl[_id] += _value; // No need to overflow check, as _updateUserTokens() warrants it.
                 _id = parentToken[_id];
             } while(_id != 0);
         }
