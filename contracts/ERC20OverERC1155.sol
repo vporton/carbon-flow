379f35e7e71881ae94fecb0625a0cdec92001095
Victor Porton <porton@narod.ru>
2020-10-11 04:32:19 +0300

    comment

--- contracts/ERC20OverERC1155.sol
+++ contracts/ERC20OverERC1155.sol
@@ -7,6 +7,7 @@ import "./IERC20.sol";
 
 interface IMyERC1155 is IERC1155, IERC1155Views { }
 
+// This contract has a bug: It does not emit ERC-20 events.
 contract ERC20OverERC1155 is IERC20
 {
     IMyERC1155 erc1155;
