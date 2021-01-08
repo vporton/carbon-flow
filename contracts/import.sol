753704a56bacd28405ff4abf9a4d2a073fbcd66f
Victor Porton <porton@narod.ru>
2021-01-08 06:47:40 +0200

    minor bug fix

--- contracts/import.sol
+++ contracts/import.sol
@@ -1,2 +1,5 @@
+//SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.7.3;
+
 import "@vporton/wrap-tokens/contracts/ERC20OverERC1155.sol";
 import "@vporton/wrap-tokens/contracts/ERC20LockedERC1155.sol";
